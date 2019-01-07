3d94df94b65d8736c3f9a86504ec9aff09a617a1
wouerner <wouerner@protonmail.com>
2019-01-07 14:32:49 -0200

    [init]

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <main role="main" class="  pt-3 px-4">
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+        <h1 class="h2">Dashboard</h1>
+        <div class="btn-toolbar mb-2 mb-md-0"></div>
+      </div>
+        <div class="row">
+            <div class="col">
+              <VueApexCharts 
+                  type="donut" 
+                  :options="chartOptions"
+                  :series="series"
+                  width="450"
+              >
+              </VueApexCharts>
+            </div>
+            <div class="col">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Hoje</h5>
+                    <p class="card-text">R$ 1000,00</p>
+                  </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" >
+                  <div class="card-body">
+                    <h5 class="card-title">Lucro</h5>
+                    <p class="card-text"> R$ 1000,00</p>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import VueApexCharts from 'vue-apexcharts'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  components:{VueApexCharts},
+  data() {
+        return {
+            series: [44, 55, 13, 43, 22],
+            chartOptions: {
+                labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+            }
+        }
   }
+
 }
 </script>
